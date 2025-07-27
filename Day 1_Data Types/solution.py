6d71d632d3fcc72f0ccea80b9affc9ab7dd73d5c
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-07-27 22:17:40 +0530

    Create solution.py
    This is the solution of Day 1....

---
+++ Day 1_Data Types/solution.py
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Predefined variables
+# Read input from stdin
+int_input = int(input())         
+double_input = float(input())    
+string_input = input()           
+
+# Perform operations and print results
+print(i + int_input)                              
+print("{:.1f}".format(d + double_input))          
+print(s + string_input)                           
