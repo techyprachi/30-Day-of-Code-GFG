d912861c090a2015f003f49ccc48a073e597fced
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-20 22:03:59 +0530

    Create solution.py
    This is the solution of Day 27 ( python 3 language used ) !!

---
+++ Day27_Testing/solution.py
@@ -0,0 +1,70 @@
+def minimum_index(seq):
+    if len(seq) == 0:
+        raise ValueError("Cannot get the minimum value index from an empty sequence")
+    min_idx = 0
+    for i in range(1, len(seq)):
+        if seq[i] < seq[min_idx]:
+            min_idx = i
+    return min_idx
+class TestDataEmptyArray:
+    @staticmethod
+    def get_array():
+        return []
+
+
+class TestDataUniqueValues:
+    @staticmethod
+    def get_array():
+        return [5, 3, 8, 6]  # All unique values
+
+    @staticmethod
+    def get_expected_result():
+        return 1  # Index of minimum value (3)
+
+
+class TestDataExactlyTwoDifferentMinimums:
+    @staticmethod
+    def get_array():
+        return [4, 2, 7, 2, 9]  # Two minimums (2) at index 1 and 3
+
+    @staticmethod
+    def get_expected_result():
+        return 1  # First occurrence of minimum
+
+
+def TestWithEmptyArray():
+    try:
+        seq = TestDataEmptyArray.get_array()
+        result = minimum_index(seq)
+    except ValueError as e:
+        pass
+    else:
+        assert False
+
+
+def TestWithUniqueValues():
+    seq = TestDataUniqueValues.get_array()
+    assert len(seq) >= 2
+
+    assert len(list(set(seq))) == len(seq)
+
+    expected_result = TestDataUniqueValues.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+
+def TestiWithExactyTwoDifferentMinimums():
+    seq = TestDataExactlyTwoDifferentMinimums.get_array()
+    assert len(seq) >= 2
+    tmp = sorted(seq)
+    assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
+
+    expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+TestWithEmptyArray()
+TestWithUniqueValues()
+TestiWithExactyTwoDifferentMinimums()
+print("OK")
+
