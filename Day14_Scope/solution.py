3d7854df6a74d7869ac831028a4c2ce6e6a689fc
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-21 14:58:07 +0530

    Update solution.py
    This is the solution of Day 14 ( python 3 language used ) !!

--- Day14_Scope/solution.py
+++ Day14_Scope/solution.py
@@ -1,10 +1,10 @@
 class Difference:
     def __init__(self, a):
         self.__elements = a
-
+        self.maximumDifference = 0
+	# Add your code here
     def computeDifference(self):
         self.maximumDifference = max(self.__elements) - min(self.__elements)
-
 # End of Difference class
 
 _ = input()
