31c2d741f9d5ab8e15636dc00c9495c50863e736
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-21 14:37:15 +0530

    Update solution.py
    This is the solution of Day 10 (python 3 language used ) !!

--- Day10_Binary Numbers/solution.py
+++ Day10_Binary Numbers/solution.py
@@ -6,10 +6,16 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     n = int(input().strip())
+
+    # convert number to binary (without '0b')
     binary = bin(n)[2:]
-    max_ones = max(map(len, binary.split('0')))
-    print(max_ones)
+
+    # split by '0' to get groups of consecutive '1's
+    groups = binary.split('0')
+
+    # find max length of consecutive '1's
+    max_consecutive_ones = max(len(group) for group in groups)
+
+    print(max_consecutive_ones)
