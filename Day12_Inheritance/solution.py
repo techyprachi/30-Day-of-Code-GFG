f3e5441f5ac95eebb66820b682ce15aac3a140a4
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-21 14:46:39 +0530

    Update solution.py
    This is the solution of Day 12 ( python 3 language used ) !!

--- Day12_Inheritance/solution.py
+++ Day12_Inheritance/solution.py
@@ -1,12 +1,14 @@
 class Person:
-    def __init__(self, firstName, lastName, idNumber):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.idNumber = idNumber
-
-    def printPerson(self):
-        print("Name:", self.lastName + ",", self.firstName)
-        print("ID:", self.idNumber)
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+        
+        
+        
 
 class Student(Person):
     def __init__(self, firstName, lastName, idNumber, scores):
@@ -14,29 +16,46 @@ class Student(Person):
         self.scores = scores
 
     def calculate(self):
-        average = sum(self.scores) / len(self.scores)
-        if average >= 90:
-            return 'O'  # Outstanding
-        elif average >= 80:
-            return 'E'  # Exceeds Expectations
-        elif average >= 70:
-            return 'A'  # Acceptable
-        elif average >= 55:
-            return 'P'  # Poor
-        elif average >= 40:
-            return 'D'  # Dreadful
+        avg = sum(self.scores) / len(self.scores)
+        if avg >= 90:
+            return 'O'
+        elif avg >= 80:
+            return 'E'
+        elif avg >= 70:
+            return 'A'
+        elif avg >= 55:
+            return 'P'
+        elif avg >= 40:
+            return 'D'
         else:
-            return 'T'  # Troll
+            return 'T'
 
-# Input handling
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        avg = sum(self.scores) / len(self.scores)
+        if avg >= 90:
+            return 'O'
+        elif avg >= 80:
+            return 'E'
+        elif avg >= 70:
+            return 'A'
+        elif avg >= 55:
+            return 'P'
+        elif avg >= 40:
+            return 'D'
+        else:
+            return 'T'
+
 line = input().split()
 firstName = line[0]
 lastName = line[1]
 idNum = line[2]
-numScores = int(input())  # Not used directly, but part of input format
-scores = list(map(int, input().split()))
-
-# Create Student object and display results
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
 s = Student(firstName, lastName, idNum, scores)
 s.printPerson()
 print("Grade:", s.calculate())
