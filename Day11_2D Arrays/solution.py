e2c9a4e432c08e1ea88c6d33c901e0e91ad6b29c
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-21 14:41:00 +0530

    Update solution.py
    This is the solution of Day 11 ( python 3 language used ) !!

--- Day11_2D Arrays/solution.py
+++ Day11_2D Arrays/solution.py
@@ -6,23 +6,22 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
-
     arr = []
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    max_sum = -63
+    max_sum = -63  # minimum possible (-9 * 7)
     
-    for i in range(4):
-        for j in range(4):
-            top = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-            mid = arr[i+1][j+1]
-            bottom = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
-            hourglass = top + mid + bottom
-            max_sum = max(max_sum, hourglass)
-            
-    print(max_sum)        
+    for i in range(4):       # rows 0..3
+        for j in range(4):   # cols 0..3
+            # calculate current hourglass sum
+            total = (
+                arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                + arr[i+1][j+1]
+                + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            )
+            max_sum = max(max_sum, total)
+
+    print(max_sum)
