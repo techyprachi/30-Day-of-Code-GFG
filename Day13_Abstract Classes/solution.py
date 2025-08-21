871afdeb1a618ccc8bd787a549006c7cc5191232
Prachi Agarwal <136839311+techyprachi@users.noreply.github.com>
2025-08-21 14:49:14 +0530

    Update solution.py
    This is the solution of Day 13 (python 3 language used ) !!

--- Day13_Abstract Classes/solution.py
+++ Day13_Abstract Classes/solution.py
@@ -11,11 +11,11 @@ class MyBook(Book):
     def __init__(self, title, author, price):
         super().__init__(title, author)
         self.price = price
-    
+
     def display(self):
-        print(f"Title: {self.title}")
-        print(f"Author: {self.author}")
-        print(f"Price: {self.price}")    
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Price:", self.price)
 
 title=input()
 author=input()
